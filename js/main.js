6ff0fff9bcc48ba8ceec96b28228b494913f6a89
Sambillingham <sam.billingham@steer.io>
2015-07-10 10:54:39 +0100

    snappy snap snap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 // js with steer
 
 // this should always store the current card on show
-var currentCard;
+var oldCard;
+var newCard;
 var timerID;
 var availableCards = generateDeck();
 var playedCards = [];
+var playingNewHand;
+var pointsAvailible = 0;
+var players = {
+	one : {
+		name: prompt('Player 1 name'),
+		score: 0
+	},
+	two : {
+		name : prompt('Player 2 name'),
+		score: 0
+	}
+}
+
 console.log('Cards: ', availableCards);
 
 function generateDeck(){
 	// generates a new deck of 52 cards 
 	//  e.g [{ suit : 'spade', face : 1 },
-	//  	{ suit : 'heart', face : 1 }... ]
+	//  	{ suit : 'heart', face : 12 }... ]
 
 	var deck = _.flatten( _.map( ['spade', 'club', 'heart', 'diamond'], function(suitValue, suitIndex, suits ){
 
@@ -25,35 +39,24 @@ function generateDeck(){
 	return deck;
 }
 
+function playNewHand() {
+	// so the loop knows not to move this old card
+	playingNewHand = true;
 
+	// Get a new card 
+	oldCard = dealCard();
 
-function playGame() {
-
-	var player = {
-		name: 'Katgrog'
-	}
-
-	// - 2 cards
-	// - 1 player 
-	console.log( availableCards[1] );
-	// - What are the cards? 
-	console.log( Math.ceil( Math.random()* 51 )); // gets a random number between 1 - 13 
-	// - Get a new card (random)
-	currentCard = newCard();
-	console.log('new rando card: Boom ->', newCard());
-
-	// run through the whole deck of cards
-	// turn over the next card one by one
-	// stop the looping if cards are the same
+	// kick things off
 	timerID = loopOverDeck();
+
+	//increment the points
+	pointsAvailible++
 }
  
-function newCard() {
+function dealCard() {
 	// - new card to display < a function 
 	var cardIndex = Math.ceil( Math.random() * ( availableCards.length - 1 ) );
 	var card = availableCards[cardIndex];
-	console.log('CARD:', card);
-
 	// remove card from deck
 	availableCards.splice(cardIndex, 1);
 	// add new card to the list of played cards
@@ -62,58 +65,114 @@ function newCard() {
  	return card;
 }
 
-function checkCards(oldCard, newCard){
-	// === checks the value and the type are the same
+function checkCards(oldCard, newCard, player){
+	console.log('old: ', oldCard,'new: ', newCard)
 
-	console.log('old card: ', oldCard);
-	console.log('new card: ', newCard);
-	// - is the second card the same or different? 
 	// - if the card is the same:
-	// - how do I know what card is what?
-	// - new card < stored above 
-	// - last card < the one before you put down the new card
 	if ( oldCard.face === newCard.face ) {
 
 		console.log("Woohoo!")
 
-		// steps if card are the same
-
-		// get rid of deck
 		// stop timer
+		clearInterval(timerID);
+
 		// give points to the winner
+		players[player].score += pointsAvailible;
+
+		// set the pot back to zero
+		pointsAvailible = 0;
+
+		// Who scored
+		console.log(players[player].name + ' scored!')
 
 		// start timer/loop again 	
+		playNewHand();
 	} else {
 		console.log('not the same :(');
 		// do nothing 
 	}
-	// store new card as current card
 }
 
 function loopOverDeck(){
 
 	// returns the id of the interval so it can be stoped with clearInterval()
 	return setInterval( function(){
-			
-		// if not last card 
-		if(playedCards.length < 52 ){
-			// get the next card from the top of the deck.
-			var nextCard = newCard();
 
-			// checks the current cards in play
-			checkCards(currentCard, nextCard);
+		// start of the 1000ms
+		// update cards on show
+		if(playingNewHand){
+			console.log('first');
+			playingNewHand = false;
+			//  dont switch the cards out as their is only an old card at this point
 		} else {
-			console.log('Game Over!');
+			oldCard = newCard;
+			// newCard = '';	
 		}
+		
+		setTimeout( function(){
+			// end of the 1000ms
 
+			// if not last card 
+			if(playedCards.length < 52 ){
+				// get the next card from the top of the deck.
+				newCard = dealCard();
 
-	}, 1000);
+				//increment the points
+				pointsAvailible++
+
+				// visually show the new card
+				updateCardsOnScreen();
+			} else {
+				endGame();
+			}
+		}, 1000);
+
+	}, 1500);
 
 }
 
-// - click on deck to start game
-playGame();
+function updateCardsOnScreen(){
+	$('.old__card__content').text(oldCard.suit + ' | ' + oldCard.face);
+	$('.new__card__content').text(newCard.suit + ' | ' + newCard.face);
+}
 
+function endGame(){
+	console.log('Game Over!');
+	
+	// 
+	var winner = _.max( players, function(players){
+		return players.score;
+	});
+
+	console.log(winner.name +  ' WON!');
+	console.log('scores: ', players);
+
+
+	// STOP
+	clearInterval(timerID);
+}
+
+
+// - click on deck to start game?
+playNewHand();
+
+// On audio -
+// checks the current cards in play
+$('body').on('keyup', function(){
+
+	var player;
+	switch(event.which) {
+		case 13 :
+			player = 'one';
+			checkCards(oldCard, newCard, player);
+		break;
+		case 32 :
+			player = 'two';
+			checkCards(oldCard, newCard, player);
+		break;
+	}
+
+});
 
 
 console.log( 'INDEX:', _.findWhere( availableCards, {suit: "spade", face: 13}) );
